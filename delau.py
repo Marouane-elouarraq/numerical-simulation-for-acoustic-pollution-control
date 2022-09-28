3877f81d7b3cab775fd6b2af5800d0d6698c9a25
Sakuragi-crypto <108550029+Sakuragi-crypto@users.noreply.github.com>
2022-09-28 10:07:17 +0200

    Update delau.py

--- delau.py
+++ delau.py
@@ -1,5 +1,6 @@
 import numpy as np
-points = np.array([[0, 0], [0, 1.1], [1, 0], [1, 1]])#, [0.5, 1.5], [0.8, 0.8]] )
+# points = np.array([[0, 0], [0, 1.1], [1, 0], [1, 1], [0.5, 1.5], [0.8, 0.8]])
+points = np.array([[0, 0], [2, 0], [0.3, 1], [2, 1], [0, 2], [0.3, 2], [-2, -1], [4, -1], [1, 5]])
 from scipy.spatial import Delaunay
 #tri = Delaunay(points)
 tri = Delaunay(points, furthest_site=False, incremental=True, qhull_options=None)
